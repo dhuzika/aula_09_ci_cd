2af4a62125e0e3be918b956496ec1315d271223f
dhuzika <72637953+dhuzika@users.noreply.github.com>
2023-05-03 09:01:27 -0300

    Update test_methods.py

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,27 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_volume():
+    # given a length of 4, width of 2 and a height of 5
+    length = 4
+    width = 2
+    height = 5
+
+    # when we calculate the volume
+    output = methods.volume_of_parallelepiped(length, width, height)
+    
+    # then the volume should be 40
+    assert output == 40
+
+def test_surface_area_of_parallelepiped(length, width, height):
+    # given a length of 4, width of 2 and a height of 5
+    length = 4
+    width = 2
+    height = 5
+
+    # when we calculate the surface
+    output = methods.surface_area_of_parallelepiped(length, width, height)
+    
+    # then the surface area should be 76
+    assert output == 76
