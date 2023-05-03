25663de3f8866d8e073727f7579c113cb92b87e6
dhuzika <72637953+dhuzika@users.noreply.github.com>
2023-05-03 09:07:58 -0300

    Update methods.py

--- methods.py
+++ methods.py
@@ -5,3 +5,11 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def volume_of_parallelepiped(length, width, height):
+    volume = length*width*height
+    return volume
+
+def test_surface_area_of_parallelepiped(length, width, height):
+    surface = 2*(length*width+width*height+length*height)
+    return surface
